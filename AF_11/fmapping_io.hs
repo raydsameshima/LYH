38c6b33d4ed6bef409cbadd9bd46861931e1b3d3
raydsameshima <rsameshima@citytech.cuny.edu>
2015-09-12 16:20:23 -0400

    fmap trial

---
+++ AF_11/fmapping_io.hs
@@ -0,0 +1,14 @@
+-- fmaping_io.hs
+
+import Data.Char
+import Data.List
+
+main = do
+  line <- fmap (intersperse '-' . reverse . map toUpper) getLine
+  putStrLn line
+
+{-
+$ runhaskell fmapping_io.hs 
+Hello Haskell
+L-L-E-K-S-A-H- -O-L-L-E-H
+-}
